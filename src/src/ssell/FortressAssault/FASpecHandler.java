1bc2e1c3c8ad1a820a4b79d7cceff8a9b800cc6b
k4b4l74 <guillaume.ettori@gmail.com>
2011-04-14 10:18:42 +0200

    Add more information to the SpecHandler

--- src/src/ssell/FortressAssault/FASpecHandler.java
+++ src/src/ssell/FortressAssault/FASpecHandler.java
@@ -1,8 +1,59 @@
 package ssell.FortressAssault;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import org.bukkit.Location;
+import org.bukkit.entity.Player;
 
 public class FASpecHandler {
 
-	public Location location;
+	private Location observersSpawnLocation;
+	private List<Player> observers;
+	
+	public FASpecHandler() {
+		observersSpawnLocation = null;
+		observers = new ArrayList<Player>();
+	}
+	
+	public Location getLocation() {
+		return observersSpawnLocation;
+	}
+	
+	public void setLocation(Location location) {
+		observersSpawnLocation = location;
+	}
+	
+	public void removeObserver(Player player) {
+		for (Iterator<Player> iterator = observers.iterator(); iterator.hasNext();) {
+			Player observer = iterator.next();
+			if (observer.getName().equalsIgnoreCase(player.getName())) {
+				iterator.remove();
+			}
+		}
+	}
+	
+	public List<Player> getObservers() {
+		return observers;
+	}
+	
+	public void addObserver(Player player) {
+		observers.add(player);
+	}
+	
+	public boolean isAnObserver(Player player) {
+		for (Iterator<Player> iterator = observers.iterator(); iterator.hasNext();) {
+			Player observer = iterator.next();
+			if (observer.getName().equalsIgnoreCase(player.getName())) {
+				return true;
+			}
+			
+		}
+		return false;
+	}
+	
+	public void clearObservers() {
+		observers = new ArrayList<Player>();
+	}
 }
